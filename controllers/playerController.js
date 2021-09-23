e267ec6241962b79776fa01d5da32d924c5dd7a2
simboslice <simeonadda@gmail.com>
2021-09-22 20:53:08 -0500

    Set up player Schema and import to player Controller

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 // IMPORT PLAYER MODEL
+const Player = require('../models/player')
 
 // INDEX FOR ALL PLAYER COLLECTION -- LIST ALL PLAYERS YOU HAVE
 router.get('/', (req, res) => {
   Player.find({}, (error, allPlayers) => {
     console.log(allPlayers);
+    res.send(allPlayers)
   })
 })
