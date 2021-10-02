f468f278c5966971b1b5f034fa5a3af5c467d84c
simboslice <simeonadda@gmail.com>
2021-10-02 10:27:23 -0500

    MVP

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -114,182 +114,182 @@ router.get('/seed', (req, res) => {
       },
       img: "https://i.pinimg.com/originals/03/d9/cd/03d9cd78a10b12345247db8bfe0feabb.gif"
     },
-    {
-      first_name: 'Dikembe',
-      last_name: 'Mutombo',
-      position: 'G',
-      height_feet: 7,
-      height_inches: 2,
-      weight_pounds: 260,
-      team: {
-        abbreviation: 'DEN',
-        city: 'Denver',
-        conference: 'West',
-        division: 'Northwest',
-        full_name: 'Denver Nuggets'
-      },
-      img: "https://64.media.tumblr.com/1f49429469c81114b240ced902e7eafe/tumblr_njgq27XnMj1u41fxpo1_400.gif"
-    },
-    {
-      first_name: 'Reggie',
-      last_name: 'Miller',
-      position: 'G',
-      height_feet: 6,
-      height_inches: 7,
-      weight_pounds: 195,
-      team: {
-        abbreviation: 'IND',
-        city: 'Indiana',
-        conference: 'East',
-        division: 'Central',
-        full_name: 'Indiana Pacers'
-      },
-      img: "https://64.media.tumblr.com/fc5383928e731ebd00bfe8f72731b564/tumblr_nk5d053TSC1u41fxpo1_400.gif"
-    },
-    {
-      first_name: 'Karl',
-      last_name: 'Malone',
-      position: 'F',
-      height_feet: 6,
-      height_inches: 9,
-      weight_pounds: 258,
-      team: {
-        abbreviation: 'UTA',
-        city: 'Utah',
-        conference: 'West',
-        division: 'Northwest',
-        full_name: 'Utah Jazz'
-      },
-      img: "https://64.media.tumblr.com/b2d04f12e3e4a92c9d6e90e39d90fe72/tumblr_njw10o2cO01u41fxpo1_400.gif"
-    },
-    {
-      first_name: 'Shawn',
-      last_name: 'Kemp',
-      position: 'F',
-      height_feet: 6,
-      height_inches: 10,
-      weight_pounds: 230,
-      team: {
-        abbreviation: 'SEA',
-        city: 'Seattle',
-        conference: 'West',
-        division: 'Northwest',
-        full_name: 'Seattle Supersonics'
-      },
-      img: "https://images.squarespace-cdn.com/content/v1/507f7079e4b010bec5a03cff/1423585172655-UG21H1Q68RYMCH7H9SHR/kitron-zimri-neuschatz-shawn-kemp.gif"
-    },
-    {
-      first_name: 'John',
-      last_name: 'Stockton',
-      position: 'G',
-      height_feet: 6,
-      height_inches: 1,
-      weight_pounds: 170,
-      team: {
-        abbreviation: 'UTA',
-        city: 'Utah',
-        conference: 'West',
-        division: 'Northwest',
-        full_name: 'Utah Jazz'
-      },
-      img: 'https://64.media.tumblr.com/bb1189f34ca039e5e70f842bd3bd9e40/tumblr_njl12hvTib1u41fxpo1_400.gifv'
-    },
-    {
-      first_name: 'David',
-      last_name: 'Robinson',
-      position: 'C',
-      height_feet: 7,
-      height_inches: 1,
-      weight_pounds: 236,
-      team: {
-        abbreviation: 'SAS',
-        city: 'San Antonio',
-        conference: 'West',
-        division: 'Southwest',
-        full_name: 'San Antonio Spurs'
-      },
-      img: "https://64.media.tumblr.com/9e92688b991fa0af882411cecc9488c8/tumblr_njoqgpY87X1u41fxpo1_400.gif"
-    },
-    {
-      first_name: 'Grant',
-      last_name: 'Hill',
-      position: 'F',
-      height_feet: 6,
-      height_inches: 8,
-      weight_pounds: 225,
-      team: {
-        abbreviation: 'DET',
-        city: 'Detroit',
-        conference: 'East',
-        division: 'Central',
-        full_name: 'Detroit Pistons'
-      },
-      img: "https://64.media.tumblr.com/82d55be2cec2a58d9198b984b16ebffd/tumblr_njmv7tUcA91u41fxpo1_400.gifv"
-    },
-    {
-      first_name: 'Gary',
-      last_name: 'Payton',
-      position: 'G',
-      height_feet: 6,
-      height_inches: 4,
-      weight_pounds: 190,
-      team: {
-        abbreviation: 'SEA',
-        city: 'Seattle',
-        conference: 'West',
-        division: 'Northwest',
-        full_name: 'Seattle Supersonics'
-      },
-      img: "https://64.media.tumblr.com/75576cf6afe4f59338afaa6816c4e552/tumblr_nk77tcYvWD1u41fxpo1_400.gif"
-    },
-    {
-      first_name: 'Tim',
-      last_name: 'Hardaway',
-      position: 'G',
-      height_feet: 6,
-      height_inches: 0,
-      weight_pounds: 175,
-      team: {
-        abbreviation: 'MIA',
-        city: 'Miami',
-        conference: 'East',
-        division: 'Southeast',
-        full_name: 'Miami Heat'
-      },
-      img: "https://64.media.tumblr.com/1a53ba6d10947122e03f6dcd9163e035/tumblr_njsi17C4801u41fxpo1_400.gifv"
-    },
-    {
-      first_name: 'Charles',
-      last_name: 'Barkley',
-      position: 'F',
-      height_feet: 6,
-      height_inches: 5,
-      weight_pounds: 250,
-      team: {
-        abbreviation: 'PHX',
-        city: 'Phoenix',
-        conference: 'West',
-        division: 'Pacific',
-        full_name: 'Phoenix Suns'
-      },
-      img: "https://64.media.tumblr.com/42eee7f544a335de689c4e88636b9cf2/tumblr_nk1drzRYwj1u41fxpo1_400.gif"
-    },
-    {
-      first_name: 'Hakeem',
-      last_name: 'Olajuwon',
-      position: 'G',
-      height_feet: 7,
-      height_inches: 0,
-      weight_pounds: 256,
-      team: {
-        abbreviation: 'HOU',
-        city: 'Houston',
-        conference: 'West',
-        division: 'Southwest',
-        full_name: 'Houston Rockets'
-      },
-      img: "https://64.media.tumblr.com/53d609bbc70311ea863a7e1b53b5e718/tumblr_njcu13TYkc1u41fxpo1_400.gif"
-    },
+    // {
+    //   first_name: 'Dikembe',
+    //   last_name: 'Mutombo',
+    //   position: 'G',
+    //   height_feet: 7,
+    //   height_inches: 2,
+    //   weight_pounds: 260,
+    //   team: {
+    //     abbreviation: 'DEN',
+    //     city: 'Denver',
+    //     conference: 'West',
+    //     division: 'Northwest',
+    //     full_name: 'Denver Nuggets'
+    //   },
+    //   img: "https://64.media.tumblr.com/1f49429469c81114b240ced902e7eafe/tumblr_njgq27XnMj1u41fxpo1_400.gif"
+    // },
+    // {
+    //   first_name: 'Reggie',
+    //   last_name: 'Miller',
+    //   position: 'G',
+    //   height_feet: 6,
+    //   height_inches: 7,
+    //   weight_pounds: 195,
+    //   team: {
+    //     abbreviation: 'IND',
+    //     city: 'Indiana',
+    //     conference: 'East',
+    //     division: 'Central',
+    //     full_name: 'Indiana Pacers'
+    //   },
+    //   img: "https://64.media.tumblr.com/fc5383928e731ebd00bfe8f72731b564/tumblr_nk5d053TSC1u41fxpo1_400.gif"
+    // },
+    // {
+    //   first_name: 'Karl',
+    //   last_name: 'Malone',
+    //   position: 'F',
+    //   height_feet: 6,
+    //   height_inches: 9,
+    //   weight_pounds: 258,
+    //   team: {
+    //     abbreviation: 'UTA',
+    //     city: 'Utah',
+    //     conference: 'West',
+    //     division: 'Northwest',
+    //     full_name: 'Utah Jazz'
+    //   },
+    //   img: "https://64.media.tumblr.com/b2d04f12e3e4a92c9d6e90e39d90fe72/tumblr_njw10o2cO01u41fxpo1_400.gif"
+    // },
+    // {
+    //   first_name: 'Shawn',
+    //   last_name: 'Kemp',
+    //   position: 'F',
+    //   height_feet: 6,
+    //   height_inches: 10,
+    //   weight_pounds: 230,
+    //   team: {
+    //     abbreviation: 'SEA',
+    //     city: 'Seattle',
+    //     conference: 'West',
+    //     division: 'Northwest',
+    //     full_name: 'Seattle Supersonics'
+    //   },
+    //   img: "https://images.squarespace-cdn.com/content/v1/507f7079e4b010bec5a03cff/1423585172655-UG21H1Q68RYMCH7H9SHR/kitron-zimri-neuschatz-shawn-kemp.gif"
+    // },
+    // {
+    //   first_name: 'John',
+    //   last_name: 'Stockton',
+    //   position: 'G',
+    //   height_feet: 6,
+    //   height_inches: 1,
+    //   weight_pounds: 170,
+    //   team: {
+    //     abbreviation: 'UTA',
+    //     city: 'Utah',
+    //     conference: 'West',
+    //     division: 'Northwest',
+    //     full_name: 'Utah Jazz'
+    //   },
+    //   img: 'https://64.media.tumblr.com/bb1189f34ca039e5e70f842bd3bd9e40/tumblr_njl12hvTib1u41fxpo1_400.gifv'
+    // },
+    // {
+    //   first_name: 'David',
+    //   last_name: 'Robinson',
+    //   position: 'C',
+    //   height_feet: 7,
+    //   height_inches: 1,
+    //   weight_pounds: 236,
+    //   team: {
+    //     abbreviation: 'SAS',
+    //     city: 'San Antonio',
+    //     conference: 'West',
+    //     division: 'Southwest',
+    //     full_name: 'San Antonio Spurs'
+    //   },
+    //   img: "https://64.media.tumblr.com/9e92688b991fa0af882411cecc9488c8/tumblr_njoqgpY87X1u41fxpo1_400.gif"
+    // },
+    // {
+    //   first_name: 'Grant',
+    //   last_name: 'Hill',
+    //   position: 'F',
+    //   height_feet: 6,
+    //   height_inches: 8,
+    //   weight_pounds: 225,
+    //   team: {
+    //     abbreviation: 'DET',
+    //     city: 'Detroit',
+    //     conference: 'East',
+    //     division: 'Central',
+    //     full_name: 'Detroit Pistons'
+    //   },
+    //   img: "https://64.media.tumblr.com/82d55be2cec2a58d9198b984b16ebffd/tumblr_njmv7tUcA91u41fxpo1_400.gifv"
+    // },
+    // {
+    //   first_name: 'Gary',
+    //   last_name: 'Payton',
+    //   position: 'G',
+    //   height_feet: 6,
+    //   height_inches: 4,
+    //   weight_pounds: 190,
+    //   team: {
+    //     abbreviation: 'SEA',
+    //     city: 'Seattle',
+    //     conference: 'West',
+    //     division: 'Northwest',
+    //     full_name: 'Seattle Supersonics'
+    //   },
+    //   img: "https://64.media.tumblr.com/75576cf6afe4f59338afaa6816c4e552/tumblr_nk77tcYvWD1u41fxpo1_400.gif"
+    // },
+    // {
+    //   first_name: 'Tim',
+    //   last_name: 'Hardaway',
+    //   position: 'G',
+    //   height_feet: 6,
+    //   height_inches: 0,
+    //   weight_pounds: 175,
+    //   team: {
+    //     abbreviation: 'MIA',
+    //     city: 'Miami',
+    //     conference: 'East',
+    //     division: 'Southeast',
+    //     full_name: 'Miami Heat'
+    //   },
+    //   img: "https://64.media.tumblr.com/1a53ba6d10947122e03f6dcd9163e035/tumblr_njsi17C4801u41fxpo1_400.gifv"
+    // },
+    // {
+    //   first_name: 'Charles',
+    //   last_name: 'Barkley',
+    //   position: 'F',
+    //   height_feet: 6,
+    //   height_inches: 5,
+    //   weight_pounds: 250,
+    //   team: {
+    //     abbreviation: 'PHX',
+    //     city: 'Phoenix',
+    //     conference: 'West',
+    //     division: 'Pacific',
+    //     full_name: 'Phoenix Suns'
+    //   },
+    //   img: "https://64.media.tumblr.com/42eee7f544a335de689c4e88636b9cf2/tumblr_nk1drzRYwj1u41fxpo1_400.gif"
+    // },
+    // {
+    //   first_name: 'Hakeem',
+    //   last_name: 'Olajuwon',
+    //   position: 'G',
+    //   height_feet: 7,
+    //   height_inches: 0,
+    //   weight_pounds: 256,
+    //   team: {
+    //     abbreviation: 'HOU',
+    //     city: 'Houston',
+    //     conference: 'West',
+    //     division: 'Southwest',
+    //     full_name: 'Houston Rockets'
+    //   },
+    //   img: "https://64.media.tumblr.com/53d609bbc70311ea863a7e1b53b5e718/tumblr_njcu13TYkc1u41fxpo1_400.gif"
+    // },
   ], (err, data) => {
     if (err) {
       console.log(err);
